3c3e037c1853a6711ddd7598f003121004f6c4bb
CoderOJ <yangzhan23@mails.tsinghua.edu.cn>
2025-03-10 11:11:41 +0800

    api: add api type definition via submodule (#5)
    * api: add api type definition via submodule
    
    * dev(eslint): ignore api/index.d.ts (generated file)
    
    * dep(api): bump version
    
    * feat(api): add fetch implement
    
    * fix(.env): backend address
    
    * dev(vite): switch port to avoid conflict with backend
    
    * upd(api): enhance error prompt
    
    * dep(api): bump version to v0.1

---
+++ src/api.ts
@@ -0,0 +1,15 @@
+import { API, APICollection } from "../api"
+
+async function api_call(req: APICollection) {
+	const header = await fetch(import.meta.env.VITE_API_URL, {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify(req)
+	})
+	if (!header.ok) {
+		throw new Error(`api fatal error: ${header.status} ${header.statusText}`)
+	}
+	return header.json()
+}
+
+export const api = new API(api_call)
