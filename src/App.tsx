5565019b688c4e6b57fb168756508956f7cdc549
Early <lin_erli@outlook.com>
2025-03-28 16:35:54 +0800

    feat(login): add login(), which calls api and return token

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import type { Component } from "solid-js"
-import { LoginModal, showLoginModal } from "./component/Login"
+import { LoginModal, login } from "./component/Login"
 
 import "fomantic-ui-css/semantic.min.css"
 import "fomantic-ui-css/semantic.min.js"
 import styles from "./App.module.css"
 
 const App: Component = () => {
+	const trylogin = () => {
+		login().then(token => {
+			console.log("登录成功，token:", token);
+		}).catch(err => {
+			console.log("登录失败:", err);
+		});
+	};
+
 	return (
 		<>
 			<div class={styles.App}>
 				<p>
 					Edit <code>src/App.tsx</code> and save to reload.
 				</p>
-				<button onClick={showLoginModal}>登录</button>
+				<button onClick={trylogin}>登录</button>
 			</div>
 			<LoginModal/>
 		</>
--- src/component/Login.tsx
+++ src/component/Login.tsx
@@ -1,14 +1,9 @@
-import { type Component } from "solid-js";
+import { type Component, createSignal } from "solid-js";
+import { api } from "../api";
+
+const [errorMessage, setErrorMessage] = createSignal("");
 
 export const LoginModal: Component = () => {
-	function login() {
-		const username = $("#login-modal input[name='username']").val();
-		const password = $("#login-modal input[name='password']").val();
-	
-		/// TODO
-		console.log(username, password);
-	}
-
 	return (
 		<div class="ui modal" id="login-modal">
 			<i class="close icon"/>
@@ -26,13 +21,57 @@ export const LoginModal: Component = () => {
 						<input type="password" name="password" placeholder="密码"/>
 					</div>
 					<div class="field"></div>
-					<div class="ui button" onClick={login}>提交</div>
+					<div class="ui button" id="login-submit">提交</div>
+					<div class="ui error message" id="error-message">
+						<div class="header">
+							登录失败
+						</div>
+						<p>{errorMessage()}</p>
+					</div>
 				</form>
 			</div>
 		</div>
 	);
 }
 
-export function showLoginModal() {
+function showLoginModal() {
 	$("#login-modal").modal("show");
+}
+
+export function login() {
+	return new Promise((res, rej) => {
+		showLoginModal();
+
+		const submit = async () => {
+			const username = String($("#login-modal input[name='username']").val());
+			const password = String($("#login-modal input[name='password']").val());
+
+			let token = "";
+			try {
+				const loginResponse = await api.login({ username, password });
+				token = loginResponse.token;
+				console.log("登录成功，token:", token);
+				removeObserver();
+				return res(token);
+			} catch (err) {
+				setErrorMessage(String(err));
+				$("#error-message").show();
+				console.log("登录失败:", err);
+			}
+		};
+		$("#login-submit").on("click", submit);
+
+		const cancelObserver = new MutationObserver(() => {
+			if ($("#login-modal").hasClass("hidden")) {
+				removeObserver();
+				rej(new Error("登录被取消"));
+			}
+		});
+		cancelObserver.observe($("#login-modal")[0], { attributes: true, attributeFilter: ["class"] });
+
+		const removeObserver = () => {
+			$("#login-submit").off("click", submit);
+			cancelObserver.disconnect();
+		}
+	});
 }
