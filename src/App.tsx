1450e6bc3fc4cdee8e96b48f9039f2da57701c68
CoderOJ <coderoj@outlook.com>
2025-03-31 11:04:26 +0800

    fix(femantic-push): adjust element layout to support push

--- src/App.tsx
+++ src/App.tsx
@@ -10,17 +10,19 @@ import styles from "./App.module.css"
 const App: Component = () => {
 	return (
 		<>
-			<TopBar/>
-			<SideBar/>
-			<div class="ui container" style="margin-top: 53px; height: calc(100% - 53px); overflow-y: auto;">
-				<div class={styles.App}>
-					<p>
-						Edit <code>src/App.tsx</code> and save to reload.
-					</p>
+			<SideBar />
+			<div class="pusher">
+				<TopBar />
+				<div class="ui container" style="margin-top: 53px; height: calc(100% - 53px); overflow-y: auto;">
+					<div class={styles.App}>
+						<p>
+							Edit <code>src/App.tsx</code> and save to reload.
+						</p>
+					</div>
 				</div>
+				<LoginModal />
+				<RegisterModal />
 			</div>
-			<LoginModal/>
-			<RegisterModal/>
 		</>
 	)
 }
