dce4ded2302fd96bf2ea04efee5dc1deab94203d
CoderOJ <coderoj@outlook.com>
2025-04-21 16:58:06 +0800

    upd(Home): fetch data from api

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,8 @@
-import { Component, createEffect, createSignal, JSXElement, Show } from "solid-js"
+import { Component, createResource, createSignal, JSXElement, Show } from "solid-js"
 import { db } from "../db"
 import { match } from "ts-pattern"
-import { Rooms, Spare, Spares, User } from "../api"
-import { Loader } from "../lib/common"
+import { api, Spare, Spares, User } from "../api"
+import { ErrorMessage, Loader } from "../lib/common"
 import { Calendar } from "../component/Calendar"
 import { format, formatDate } from "date-fns"
 import { zhCN } from "date-fns/locale"
@@ -63,43 +63,15 @@ const AvailableSpares = (props: { spares: Spares }) => (
 	</div>
 )
 
-const demo_spares = (week: Date): Spares => [
-	{
-		id: 10001,
-		stamp: 1,
-		week: format(week, "RRRR-'W'II"),
-		// ISO 8601 time diff format, begin_time 8hrs, end_time 9hrs30mins
-		begin_time: "PT08H00M00S",
-		end_time: "PT09H30M00S",
-		room: "205",
-		assignee: {
-			id: 3,
-			username: "zhangyang",
-		},
-	},
-	{
-		id: 10002,
-		stamp: 1,
-		week: format(week, "RRRR-'W'II"),
-		// ISO 8601 time diff format, begin_time 1days8hrs, end_time 9hrs30mins
-		begin_time: "P1DT08H00M00S",
-		end_time: "P1DT09H30M00S",
-		room: "208",
-		assignee: undefined,
-	},
-]
-
 const Main = (props: { user: User }) => {
 	// TODO: Add Week Selector
 	// (use date-fns, getISOWeek)
 
 	const [week, set_week] = createSignal(new Date())
-	const [data, set_data] = createSignal<{ spares: Spares, rooms: Rooms }>()
-	createEffect(() => {
-		set_data(undefined)
-		const current_week = week();
-		(new Promise(resolve => setTimeout(resolve, 1000))).then(() => {
-			set_data({ spares: demo_spares(current_week), rooms: ["205", "208"] })
+	const [data] = createResource(week, async () => {
+		return api.spare_list({
+			type: "Week",
+			content: format(week(), "RRRR-'W'ww"),
 		})
 	})
 
@@ -107,15 +79,22 @@ const Main = (props: { user: User }) => {
 		<div class="ui" style={{ "text-align": "center" }}>
 			<WeekSelect get={week} set={set_week} />
 		</div>
-		{
-			match(data())
-				.with(undefined, () => <Loader />)
-				.otherwise(data => <>
-					<Calendar spares={data.spares} rooms={data.rooms} base_week={week()} focus_user={props.user} />
-					<MySpares spares={data.spares.filter(spare => spare.assignee?.id === props.user.id)} />
-					<AvailableSpares spares={data.spares.filter(spare => spare.assignee === undefined)} />
-				</>)
-		}
+		{(() => {
+			if (data.error) {
+				return <ErrorMessage message={data.error.toString()} />
+			}
+			else if (data.loading || data() === undefined) {
+				return <Loader />
+			}
+			else {
+				const { spares, rooms } = data()!
+				return <>
+					<Calendar spares={spares} rooms={rooms} base_week={week()} focus_user={props.user} />
+					<MySpares spares={spares.filter(spare => spare.assignee?.id === props.user.id)} />
+					<AvailableSpares spares={spares.filter(spare => spare.assignee === undefined)} />
+				</>
+			}
+		})()}
 	</>
 }
 
